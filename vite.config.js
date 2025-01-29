5de3c34a56a9afa6d27a29237e704590c44c23ec
faresgst <gasti.fares444@gmail.com>
2025-01-29 10:46:34 +0100

    fares_importCsv front

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,6 +14,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server : {
+    port:3000,
+    proxy: {
+      '/api':{
+        target:'http://localhost:5000',
+        changeOrigin:true,
+      },
     },
   },
 })
